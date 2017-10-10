67fc0a1fd04a1332bc7907a1b85f43104f10f6fe
Igor Sharonov <igsha@users.noreply.github.com>
2017-10-10 22:35:12 +0300

    Adds save buttons

--- visualization.js
+++ visualization.js
@@ -1,31 +1,54 @@
 'use strict';
 
-function putResultsInTable(header, content) {
-    var tbl  = document.getElementById('output');
-    tbl.innerHTML = '';
-
-    var tr = tbl.insertRow();
-    for (var x of header) {
-        var td = tr.insertCell();
-        td.innerHTML = x;
-    }
+function putResultsInTable(content) {
+    var tbody = document.createElement('tbody');
 
     for (var i in content) {
-        var tr = tbl.insertRow();
+        var tr = tbody.insertRow();
         for (var j in content[i]) {
             var td = tr.insertCell();
             var value = content[i][j];
             if (typeof(value) === 'number' && (value % 1) != 0)
                 value = value.toFixed(2) + '%'
-            td.innerHTML = value;
+            td.innerText = value;
         }
     }
+
+    var tbl  = document.getElementById('output');
+    if (tbl.tBodies.length == 0)
+        tbl.appendChild(tbody);
+    else
+        tbl.tBodies[0].replaceWith(tbody);
 }
 
 function visualize(words) {
-    var header = ['Word', 'Frequency', 'Count', 'Stem'];
-    putResultsInTable(header, words);
+    putResultsInTable(words);
 
     var label = document.getElementById('label');
     label.innerHTML = 'Statistic: ' + words.length + ' words, ' + words.reduce((a, x) => a + x.count, 0) + ' total words in the text.';
+
+    document.getElementById('b-csv').disabled = false;
+    document.getElementById('b-text').disabled = false;
+}
+
+function downloadCsv(table) {
+    var text = '';
+    for (var i = 0; i < table.rows.length; i++) {
+        var cells = [];
+        for (var j = 0; j < table.rows[i].cells.length; j++)
+            cells.push(table.rows[i].cells[j].innerText);
+
+        text += cells.join(',') + '\n';
+    }
+
+    download(text, 'Words.csv', 'text/csv');
+}
+
+function downloadText(table) {
+    var text = '';
+    var rows = table.tBodies[0].rows;
+    for (var i = 0; i < rows.length; i++)
+        text += rows[i].cells[0].innerText + '\n';
+
+    download(text, 'Words.txt', 'text/plain');
 }
